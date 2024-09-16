2448d3235d290963fe72f8b98dab3879445e3503
Jamie Bliss <jamie@ivyleav.es>
2024-09-16 19:28:42 -0400

    Fix viztests/text_shared_font

--- viztests/text_shared_font.py
+++ viztests/text_shared_font.py
@@ -1,13 +1,16 @@
-import ppb
+from ppb import Vector, GameEngine, Scene
+from ppb_sdl2.sprites import Sprite
+from ppb_sdl2.systems import Image, Renderer, EventPoller, Font, Text
 
-font = ppb.Font("resources/ubuntu_font/Ubuntu-R.ttf", size=72)
-my_first_text = ppb.Text("My first text", font=font, color=(255, 255, 255))
-my_second_text = ppb.Text("My second text", font=font, color=(255, 255, 255))
+font = Font("resources/ubuntu_font/Ubuntu-R.ttf", size=72)
+my_first_text = Text("My first text", font=font, color=(255, 255, 255))
+my_second_text = Text("My second text", font=font, color=(255, 255, 255))
 
 
 def setup(scene):
-    scene.add(ppb.Sprite(image=my_first_text))
-    scene.add(ppb.Sprite(image=my_second_text, position=ppb.Vector(0, -2)))
+    scene.add(Sprite(image=my_first_text))
+    scene.add(Sprite(image=my_second_text, position=Vector(0, -2)))
 
 
-ppb.run(setup)
+with GameEngine(Scene, systems=[EventPoller, Renderer], scene_kwargs={"set_up": setup}, resolution=(800, 600)) as eng:
+    eng.run()
